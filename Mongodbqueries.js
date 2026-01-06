73f0dbe128c8ce849651b9a921f1a66a529f23b1
ThennarasiMadhuraj <thenedu@gmail.com>
2026-01-06 18:51:59 +0530

    Refactor MongoDB queries 3, 4, and 6

--- Mongodbqueries.js
+++ Mongodbqueries.js
@@ -25,17 +25,17 @@ db.company_drives.find({
 
 
 // 3.Find all the company drives and students who are appeared for the placement.
-db.company_drives.aggregate([
-  {$match:{ student_attended: true}},
-  {$project:{ userid:1, company_name:1}}
-])
+db.company_drives.find(
+  { students_attended: { $ne: [] } },
+  { company_name: 1, students_attended: 1 }
+)
 
 
 // 4. Find the number of problems solved by the user in codekata
-db.codekata.aggregate([
-  { $match: { userid: 1 } },
-  { $project: { userid: 1, problems_solved: 1} }
-])
+db.codekata.find(
+  { userid: 1 },
+  { userid: 1, problems_solved: 1 }
+)
 
 
 // 5. Find all the mentors with who has the mentee's count more than 15
@@ -46,20 +46,37 @@ db.mentors.find({
 
 
 // 6. Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
-db.attendance.find({
-  status: "absent",
-  date: {
-    $gte: ISODate("2020-10-15"),
-    $lte: ISODate("2020-10-31")
+db.attendance.aggregate([
+  {
+    $match: {
+      status: "absent",
+      date: {
+        $gte: ISODate("2020-10-15"),
+        $lte: ISODate("2020-10-31")
+      }
+    }
+  },
+  {
+    $lookup: {
+      from: "tasks",
+      localField: "userid",
+      foreignField: "userid",
+      as: "task_details"
+    }
+  },
+  { $unwind: "$task_details" },
+  {
+    $match: {
+      "task_details.submitted": false,
+      "task_details.due_date": {
+        $gte: ISODate("2020-10-15"),
+        $lte: ISODate("2020-10-31")
+      }
+    }
+  },
+  {
+    $count: "absent_and_task_not_submitted_users"
   }
-})
-
-db.tasks.find({
-  submitted: false,
-  due_date: {
-    $gte: ISODate("2020-10-15"),
-    $lte: ISODate("2020-10-31")
-  }
-})
+])
 
 //End
